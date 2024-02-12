89b515f02649326d32d5ed55352bcec76820097f
Edward <edeetee@gmail.com>
2024-02-12 15:45:03 +1300

    unstable nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs"; # Or a stable channel
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; # Or a stable channel
     utils.url = "github:numtide/flake-utils";
   };
 
@@ -15,7 +15,7 @@
           version = "3.4";
           src = pkgs.fetchurl {
             url = "https://flamenco.blender.org/downloads/flamenco-3.4-linux-amd64.tar.gz";
-            sha256 = "1185a721cf3b016b2772c28a7b4748fe73dc97d73c88b197f670b2c719e20cc4"; # Replace with the actual SHA256
+            sha256 = "1185a721cf3b016b2772c28a7b4748fe73dc97d73c88b197f670b2c719e20cc4";
           };
 
           installPhase = ''
